74f30d6d22de30b275e32ef45bb33c4016b63697
caeyla <mortelcail@gmail.com>
2025-06-08 20:55:25 +0300

    map enity to database model

--- src/main/java/magic/vente/stock/adapter/data/ItemData.java
+++ src/main/java/magic/vente/stock/adapter/data/ItemData.java
@@ -1,5 +1,6 @@
 package magic.vente.stock.adapter.data;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -9,14 +10,23 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "item")
+@Table(name = "produits")
 @Builder
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ItemData {
     @Id
+    @Column(name = "id_produit")
     private Long id;
-    private String name;
-    private String description;
+    @Column(unique = true)
+    private String reference;
+    @Column(name = "libelle")
+    private String label;
+    @Column(name = "est_du_jour")
+    private boolean isTrendToday;
+    @Column(name = "prix")
+    private double price;
+    @Column(name = "quantite_en_stock")
+    private int quantityInStock;
 }
--- src/main/java/magic/vente/stock/controller/ItemController.java
+++ src/main/java/magic/vente/stock/controller/ItemController.java
@@ -5,6 +5,7 @@ import magic.vente.stock.domain.service.ItemRetrieveService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -16,11 +17,14 @@ public class ItemController {
     private final ItemRetrieveService itemRetrieveService;
 
     @GetMapping
-    public ResponseEntity retrieveAllItems() {
+    public ResponseEntity retrieveAllItems(
+            @RequestParam(name = "limit", defaultValue = "10") int limit,
+            @RequestParam(name = "offset", defaultValue = "0") int offset
+    ) {
         return itemRetrieveService.retrieveAll(
-            Map.of(), // Assuming no filters are applied for simplicity
-            10, // Default limit
-            0 // Default offset
+            Map.of(),
+            limit,
+            offset
         );
     }
 }
