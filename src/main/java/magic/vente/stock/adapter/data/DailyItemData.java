238cc5cb223d266fa69f10285ea6f081302a0b88
caeyla <mortelcail@gmail.com>
2025-06-09 17:38:50 +0300

    map db data

--- src/main/java/magic/vente/stock/adapter/data/DailyItemData.java
+++ src/main/java/magic/vente/stock/adapter/data/DailyItemData.java
@@ -10,15 +10,17 @@ import java.time.LocalDate;
 
 
 @Entity
-@Table
+@Table(name = "produit_du_jour")
 @Builder
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class DailyItemData {
     @Id
+    @Column(name="id_produit_du_jour")
     private Long id;
     @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_produit")
     private ItemData item;
     private LocalDate date;
 }
